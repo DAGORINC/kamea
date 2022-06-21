a775d2e944d1e7ed2b74be249fa6ce8dab7d7f9f
dagor100 <damianxe@onet.pl>
2022-06-21 19:43:17 +0200

    mobile debugging

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,12 +4,22 @@ import subtitleImg from '../../assets/images/subtitle.png';
 import menuIcon from '../../assets/images/menu.svg';
 
 
+function MobileNavbar() {
+    return (
+        <div>
+            <div className={styles.mobileMain}>
+            </div>
+        </div>
+    )
+}
+
 function Navbar() {
     return (
         <div className={styles.nav}>
+            <MobileNavbar />
             <div className={styles.menu}>
-                <a href="#"><img src={carImg} className={styles.carImg}/></a>
-                <a href="#" className={styles.aText}><img src={subtitleImg} className={styles.subtitleImg}/></a>
+                <a href="#"><img src={carImg} className={styles.carImg} /></a>
+                <a href="#" className={styles.aText}><img src={subtitleImg} className={styles.subtitleImg} /></a>
                 <ul>
                     <li className={styles.listPart}>
                         <a href="#kruszywa" className={styles.partLink}>Kruszywa</a>
@@ -24,13 +34,13 @@ function Navbar() {
                         <a href="#transport" className={styles.partLink}>Transport</a>
                     </li>
                     <li className={styles.listPart}>
-                    <a href="#findus" className={styles.partLink}>Gdzie nas szukać</a>
+                        <a href="#findus" className={styles.partLink}>Gdzie nas szukać</a>
                     </li>
                     <li className={styles.listPart}>
-                    <a href="#contact" className={styles.partLink}>Kontakt</a>
+                        <a href="#contact" className={styles.partLink}>Kontakt</a>
                     </li>
                 </ul>
-            <img  className={styles.menulist} src={menuIcon} />
+                <img className={styles.menulist} src={menuIcon} />
             </div>
         </div>
     );
