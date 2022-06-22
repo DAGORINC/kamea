21f0842dcc4abde28215a4641e930a8521c2f889
dagor100 <damianxe@onet.pl>
2022-06-22 14:35:28 +0200

    mobile debugging

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,12 +3,9 @@ import carImg from '../../assets/images/car.png';
 import subtitleImg from '../../assets/images/subtitle.png';
 import menuIcon from '../../assets/images/menu.svg';
 
-
 function MobileNavbar() {
     return (
         <div>
-            <div className={styles.mobileMain}>
-            </div>
         </div>
     )
 }
@@ -16,31 +13,44 @@ function MobileNavbar() {
 function Navbar() {
     return (
         <div className={styles.nav}>
+
             <MobileNavbar />
+
             <div className={styles.menu}>
+
                 <a href="#"><img src={carImg} className={styles.carImg} /></a>
                 <a href="#" className={styles.aText}><img src={subtitleImg} className={styles.subtitleImg} /></a>
+               
                 <ul>
                     <li className={styles.listPart}>
                         <a href="#kruszywa" className={styles.partLink}>Kruszywa</a>
                     </li>
+
                     <li className={styles.listPart}>
                         <a href="#nawozy" className={styles.partLink}>Nawozy</a>
                     </li>
+
                     <li className={styles.listPart}>
                         <a href="#ziemia" className={styles.partLink}>Ziemia</a>
                     </li>
+
                     <li className={styles.listPart}>
                         <a href="#transport" className={styles.partLink}>Transport</a>
                     </li>
+
                     <li className={styles.listPart}>
                         <a href="#findus" className={styles.partLink}>Gdzie nas szukać</a>
                     </li>
+
                     <li className={styles.listPart}>
                         <a href="#contact" className={styles.partLink}>Kontakt</a>
                     </li>
+
                 </ul>
-                <img className={styles.menulist} src={menuIcon} />
+
+                <img className={styles.menulist} src={menuIcon}></img>
+            <div id='mobileMain' className={styles.mobileMain}>
+            </div>
             </div>
         </div>
     );
